32b44ad5294d4556fbc3a658a58236f74079d55f
Teresa Liu <kuas033@gmail.com>
2020-01-27 20:20:30 -0800

    data get

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const inquirer = require('inquirer');
 const axios = require('axios');
 const fs = require('fs');
+const questions = [{
+    type: 'input',
+    name: 'userName',
+    message: 'What is your github username ?'
+},
+{
+    type: 'list',
+    name: 'color',
+    message: 'What is your fav color ?',
+    choices: ['pink', 'blue', 'yellow']
+}];
 
-function promptUser(){
-    return inquirer.prompt([
-        {
-            type:'input',
-            name:'name',
-            message:'What is your github username ?'
-        },
-        {
-            type:'input',
-            name:'color',
-            message:'What is your fav color ?'
-        }
+async function promptUser() {
+    try {
+        const {userName, color} = await inquirer.prompt(questions);
+        const queryUrl = `https://api.github.com/users/${userName}`;
+        const res = await axios.get(queryUrl)
+        const { avatar_url, name, location, html_url, blog, bio, public_repos, followers, following } = res.data;
+        const queryUrl2 = `https://api.github.com/users/${userName}/repos`;
+        const repos = await axios.get(queryUrl2)
+        // console.log(repos.data.length);
+        const starRepos = repos.data.filter(repo=>repo.stargazers_count > 0).length;
+        console.log(starRepos);
 
-    ]).then(answers =>{
-        console.log(answers);
-    }).catch(console.error);
+    } catch (error) {
+console.error(error);
+    }
+    
 }
 
 promptUser()
+
