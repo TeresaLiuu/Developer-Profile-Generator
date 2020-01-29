62a3b737d4f152ac67ff338ccef2e996f879db4f
Teresa Liu <kuas033@gmail.com>
2020-01-28 20:41:32 -0800

    add html content

--- index.js
+++ index.js
@@ -15,21 +15,61 @@ const questions = [{
 
 async function promptUser() {
     try {
-        const {userName, color} = await inquirer.prompt(questions);
+        const { userName, color } = await inquirer.prompt(questions);
         const queryUrl = `https://api.github.com/users/${userName}`;
         const res = await axios.get(queryUrl)
-        const { avatar_url, name, location, html_url, blog, bio, public_repos, followers, following } = res.data;
+        const userInfo = res.data;
         const queryUrl2 = `https://api.github.com/users/${userName}/repos`;
         const repos = await axios.get(queryUrl2)
-        // console.log(repos.data.length);
-        const starRepos = repos.data.filter(repo=>repo.stargazers_count > 0).length;
-        console.log(starRepos);
+        const starRepos = repos.data.filter(repo => repo.stargazers_count > 0).length;
+        // console.log(avatar_url, name, location, html_url, blog, bio, public_repos, followers, following);
+        const html = generateHtml(userInfo);
+        const css = generateCss(color)
+        fs.writeFile('index.html', html, (err) => {
+            console.log(err)
+        });
 
-    } catch (error) {
-console.error(error);
+        fs.writeFile('style.css', css, (err) => {
+            console.log(err)
+        });
     }
-    
+    catch (error) {
+        console.error(error);
+    }
+
+};
+
+function generateHtml({ avatar_url, name, location, html_url, blog, bio, public_repos, followers, following }){
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="card" style="width: 18rem;">
+                <img src="${avatar_url}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">I am from ${location}</p>
+            </div>
+        </div>
+
+    </body>
+    </html>`;
+}
+
+function generateCss(color){
+    return `
+    body {
+        background-color: ${color}
+    }
+    `
 }
 
 promptUser()
-
+    
