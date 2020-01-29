bdbf9174114b00eba534bbc964fc1222765cca14
Teresa Liu <kuas033@gmail.com>
2020-01-29 15:37:08 -0800

    add css

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer');
 const axios = require('axios');
 const fs = require('fs');
+const html = require ('./generateHtml');
+
 const questions = [{
     type: 'input',
     name: 'userName',
@@ -22,10 +24,10 @@ async function promptUser() {
         const queryUrl2 = `https://api.github.com/users/${userName}/repos`;
         const repos = await axios.get(queryUrl2)
         const starRepos = repos.data.filter(repo => repo.stargazers_count > 0).length;
-        // console.log(avatar_url, name, location, html_url, blog, bio, public_repos, followers, following);
-        const html = generateHtml(userInfo);
-        const css = generateCss(color)
-        fs.writeFile('index.html', html, (err) => {
+        const index = html.generateHtml(userInfo);
+        const css = generateCss(color);
+
+        fs.writeFile('index.html', index, (err) => {
             console.log(err)
         });
 
@@ -39,37 +41,14 @@ async function promptUser() {
 
 };
 
-function generateHtml({ avatar_url, name, location, html_url, blog, bio, public_repos, followers, following }){
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
-        <title>Document</title>
-    </head>
-    <body>
-        <div class="card" style="width: 18rem;">
-                <img src="${avatar_url}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <p class="card-text">I am from ${location}</p>
-            </div>
-        </div>
 
-    </body>
-    </html>`;
-}
-
-function generateCss(color){
+function generateCss(color) {
     return `
-    body {
+    .card {
         background-color: ${color}
-    }
+        color: white;
+    };
     `
 }
 
 promptUser()
-    
